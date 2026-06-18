0d28032777a63c9db365a9d080d7cdb439b42f3f
SonuTheCode <sy2440052@gmail.com>
2026-06-18 18:36:20 +0530

    Change API base URL to production

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://ai-interview-pro-x39z.onrender.com",
     withCredentials:true
 });
 
