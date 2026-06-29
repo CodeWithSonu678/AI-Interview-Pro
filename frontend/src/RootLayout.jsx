84971310f49445823175c4e1284715c2be01c077
SonuTheCode <sy2440052@gmail.com>
2026-06-29 14:07:36 +0530

    Update RootLayout.jsx

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -1,6 +1,6 @@
 import {Outlet} from 'react-router'
 import Navbar from './features/interview/components/Navbar/Navbar'
-import Footer from './features/interview/components/footer/footer'
+import Footer from './features/interview/components/Footer/Footer'
 import ThemeUi from './features/interview/components/Theme/ThemeUi'
 
 const RootLayout = () => {
