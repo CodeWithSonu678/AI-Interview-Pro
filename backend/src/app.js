e9403791b70dcf3d93717b9c3dd5b6166bc45c25
SonuTheCode <sy2440052@gmail.com>
2026-07-05 15:02:41 +0530

    Update CORS origin for AI interview project

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
         "http://localhost:5174",
         "https://codewithsonu678.github.io",
         "https://ai-interview-pro-iota.vercel.app",
-        "https://ai-interview-en5ldc2eb-codewithsonu678s-projects.vercel.app",
+        "https://aiinterviewpro.vercel.app",
         "https://ai-interview-pro-v16c.vercel.app"
     ],
     credentials:true
